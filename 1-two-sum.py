23afb787885a50649e721eab436350e75bd95306
AvarbyKira <1248488996@qq.com>
2023-09-22 00:25:34 +1000

    approach 2

--- 1-two-sum.py
+++ 1-two-sum.py
@@ -19,12 +19,42 @@ Space complexity:
 class Solution(object):
     def twoSum(self, nums, target):
 
-        i, j = 0, 0
+        # find the length of the bool array
+        biggestNum = 0
+        for num in nums:
+            if num > biggestNum:
+                biggestNum = num
 
+        # initialize a bool array
+        boolArray = [False] * (biggestNum+1)
+        for num in nums:
+            boolArray[num] = True
+
+        # iterate for 'nums' list
+        i = 0
         while i < len(nums):
-            while j < len(nums):
-                j = i
-                if nums[i] + nums[j] == target:
-                    return i, j
-                j += 1
-        i += 1
+            if boolArray[target - nums[i]]:
+                break
+            i += 1
+
+        j = 0
+        while j < len(nums):
+            if nums[j] == target - nums[i]:
+                break
+            j += 1
+
+        return i, j
+
+
+
+        # initial attempt: Time Limit Exceeded
+        #
+        # i, j = 0, 0
+        #
+        # while i < len(nums):
+        #     while j < len(nums):
+        #         j = i
+        #         if nums[i] + nums[j] == target:
+        #             return i, j
+        #         j += 1
+        # i += 1
