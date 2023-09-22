8600cc6ff598d03b0f06115867ac161a6d1dfbde
AvarbyKira <1248488996@qq.com>
2023-09-22 13:21:47 +1000

    ERROR: index out of range

--- 4-sum-of-tow.py
+++ 4-sum-of-tow.py
@@ -7,31 +7,29 @@ Problem Link: https://leetcode.com/problems/median-of-two-sorted-arrays/
 
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
-        mid = int(len(nums1) + len(nums2)) + 1
 
-        i, j = 0, 0
-        count = 0
         flag = False
+        if (len(nums1) + len(nums2)) % 2 == 0:
+            flag = True
 
-        while count < mid:
+        length = int((len(nums1) + len(nums2)) / 2) + 1
+
+        count = mid = midBefore = i = j = 0
+
+        while count < length:
+            midBefore = mid
             if nums1[i] < nums2[j]:
-                i += 1
-                count += 1
-                flag = True
-            else:
+                mid = nums1[i]
                 j += 1
-                count += 1
-                flag = False
-
-        if (len(nums1) + len(nums2)) % 2 != 0:
-            if flag:
-                return nums1[i]
             else:
-                return nums2[j]
-        else:
-            if flag:
-                return nums[i]
+                mid = nums2[j]
+                i += 1
+            count += 1
 
+        if flag:
+            return (mid+midBefore) / 2
+        else:
+            return mid
 
 
         """
