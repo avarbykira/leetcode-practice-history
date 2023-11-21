68c68bc07d0a09cb408baf59cf9bdb722685dde6
AvarbyKira <1248488996@qq.com>
2023-11-21 17:30:41 +0800

    Add initial file for counting nice pairs in an array

---
+++ 1814. Count Nice Pairs in an Array.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def rev(self, num):
+        str = str(num)
+        str = str[::-1]
+        return int(str)
+    
+    def checkPair(self, num1, num2):
+        return (num1 + self.rev(num2)) == (num2 + self.rev(num1))
+
+    def countNicePairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        count = 0
+        for num1 in nums:
+            for num2 in nums:
+                if self.checkPair(num1, num2):
+                    count += 1
+
+        return count
+
