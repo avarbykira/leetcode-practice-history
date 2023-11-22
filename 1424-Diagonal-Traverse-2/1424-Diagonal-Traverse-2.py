7ffd0c004fa21cdea977970c9913d8a4d39f6dc8
AvarbyKira <1248488996@qq.com>
2023-11-22 12:06:17 +0800

    refactor: support n*m matrix

--- 1424-Diagonal-Traverse-2/1424-Diagonal-Traverse-2.py
+++ 1424-Diagonal-Traverse-2/1424-Diagonal-Traverse-2.py
@@ -2,24 +2,29 @@ class Solution(object):
     def findDiagonalOrder(self, nums):
         # use a new list to represent a full matrix
         # store -1 to empty cells
-        numsLen = len(nums)
-        listNew = []
+
+        # decide the dimention of new matrix
+        dim = len(nums)
         for list in nums:
-            innerList = [None] * numsLen
+            if len(list) > dim:
+                dim = len(list)
+
+        initialInnerList = [-1] * dim
+        listNew = [initialInnerList] * dim
+        for list in nums:
             i = 0
             for num in list:
-                innerList[i] = num
-                i += 1
-            for j in range(i, numsLen):
-                innerList[j] = -1
-            listNew.append(innerList)
+                j = 0
+                listNew[i][j] = num
+                j += 1
+            i+= 1
         
         # add them to a new list by order
         listFinal = []
-        for turn in range(0, 2*numsLen-1):
+        for turn in range(0, 2*dim-1):
             for y in range(0, turn+1):
                 x = turn - y
-                if x<numsLen and y<numsLen:
+                if x<dim and y<dim:
                     listFinal.append(listNew[x][y])
         
         # delete all -1
