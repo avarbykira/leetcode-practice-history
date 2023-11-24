3fefb4326655bf259e44aa6210f944edd4d10879
AvarbyKira <1248488996@qq.com>
2023-11-24 14:08:54 +0800

    first try

---
+++ 1561-Maximum-Number-of-Coins-You-Can-Get/1561-code.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxCoins(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: int
+        """
+        l = len(piles)
+        
+        piles.sort()
+        
+        num = 0
+        for i in range(0, (l/3)-1):
+            num += piles[l+2*i]
+
+        return num
