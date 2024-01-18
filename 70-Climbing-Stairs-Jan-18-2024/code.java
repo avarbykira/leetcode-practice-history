c042e1d4f4043b3a1b7ef97beafc613df6635d4d
AvarbyKira <1248488996@qq.com>
2024-01-18 10:53:13 +0800

    finished today's challenge

--- 70-Climbing-Stairs-Jan-18-2024/code.java
+++ 70-Climbing-Stairs-Jan-18-2024/code.java
@@ -3,11 +3,14 @@
 // these types of question has a similar pattern: 
 // if you wanna solve the ultimate problem, you can solve the downgraded one first.
 
+// tag: recursion, TLE
+
 class Solution {
     public int climbStairs(int n) {
+        if(n<=0) return 0;
         if(n==1) return 1;
         if(n==2) return 2;
-        return 2*climbStairs(n-2);
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 }
 
@@ -19,5 +22,23 @@ class Solution {
             count += a*(a+1)/2;
         }
         return count;
+    }
+}
+
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
     }
 }
